8b47e22be43b91227e21f5bcaa8397ce31e8355a
vinicius-omelo <viniciuss.uee@gmail.com>
2024-09-26 19:44:14 -0300

    commits

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 `use strict`;
 
 const images = [
-  { id: "1", url: "./src/image/one-piece-luffy.jpg" },
-  { id: "2", url: "./src/image/one-piece-law.jpg" },
-  { id: "3", url: "./src/image/one-piece-kid.jpg" },
-  { id: "4", url: "./src/image/one-piece-teach.jpg" },
-  { id: "5", url: "./src/image/one-piece-crocodile.jpg" },
+  { id: "1", url: "./src/image/one-piece-law.jpg" },
+  { id: "2", url: "./src/image/one-piece-luffy.jpg" },
+  { id: "3", url: "./src/image/one-piece-sanji.jpg" },
+  { id: "4", url: "./src/image/one-piece-katakuri.jpg" },
+  { id: "5", url: "./src/image/one-piece-jinbe.jpg" },
   { id: "6", url: "./src/image/one-piece-zoro.jpg" },
-  { id: "7", url: "./src/image/one-piece-jinbe.jpg" },
-  { id: "8", url: "./src/image/one-piece-katakuri.jpg" },
-  { id: "9", url: "./src/image/one-piece-sanji.jpg" },
+  { id: "7", url: "./src/image/one-piece-crocodile.jpg" },
+  { id: "8", url: "./src/image/one-piece-teach.jpg" },
+  { id: "9", url: "./src/image/one-piece-kid.jpg" },
 ];
 
 const containerItems = document.querySelector("#container-items");
-const containerIndicators = document.querySelector(".indicators");
-
-const createIndicators = (images, container) => {
-  images.forEach((image) => {
-    container.innerHTML += `<span data-number=${image.id}>${image.id}</span>`;
-  });
-};
 
 const loadImages = (images, container) => {
   images.forEach((image) => {
@@ -32,20 +25,8 @@ const loadImages = (images, container) => {
 };
 
 loadImages(images, containerItems);
-createIndicators(images, containerIndicators);
 
 let items = document.querySelectorAll(".item");
-
-const removeClassSelected = () => {
-  const indicators = document.querySelectorAll("span");
-  indicators.forEach((indicator) => indicator.classList.remove("selected"));
-};
-
-const selectIndicator = (number) => {
-  removeClassSelected();
-  const indicator = document.querySelector(`span[data-number="${number}"]`);
-  indicator.classList.add("selected");
-};
 
 const previous = () => {
   containerItems.appendChild(items[0]);
@@ -60,22 +41,5 @@ const next = () => {
   selectIndicator(items[1].dataset.number);
 };
 
-const clickIndicators = ({ target }) => {
-  if (target.tagName == "SPAN") {
-    const selectedIndicator = target.dataset.number;
-    let visibleSlide = items[1].dataset.number;
-    if (selectedIndicator !== visibleSlide) {
-      const autoNext = setInterval(() => {
-        document.querySelector("#next").click();
-        visibleSlide = items[1].dataset.number;
-        if (selectedIndicator == visibleSlide) clearInterval(autoNext);
-      }, 100);
-    }
-  }
-};
-
 document.querySelector("#previous").addEventListener("click", previous);
 document.querySelector("#next").addEventListener("click", next);
-document
-  .getElementById("indicators")
-  .addEventListener("click", clickIndicators);
