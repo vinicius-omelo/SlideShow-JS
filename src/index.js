8044283ae3fe23cea9b6b4e12cdfbb99b179770a
vinicius-omelo <viniciuss.uee@gmail.com>
2024-09-25 22:38:47 -0300

    upgrade2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+`use strict`;
+
 const images = [
   { id: "1", url: "./src/image/one-piece-luffy.jpg" },
   { id: "2", url: "./src/image/one-piece-law.jpg" },
@@ -10,7 +12,14 @@ const images = [
   { id: "9", url: "./src/image/one-piece-sanji.jpg" },
 ];
 
-const container = document.querySelector("#container-items");
+const containerItems = document.querySelector("#container-items");
+const containerIndicators = document.querySelector(".indicators");
+
+const createIndicators = (images, container) => {
+  images.forEach((image) => {
+    container.innerHTML += `<span data-number=${image.id}>${image.id}</span>`;
+  });
+};
 
 const loadImages = (images, container) => {
   images.forEach((image) => {
@@ -22,4 +31,51 @@ const loadImages = (images, container) => {
   });
 };
 
-loadImages(images, container);
+loadImages(images, containerItems);
+createIndicators(images, containerIndicators);
+
+let items = document.querySelectorAll(".item");
+
+const removeClassSelected = () => {
+  const indicators = document.querySelectorAll("span");
+  indicators.forEach((indicator) => indicator.classList.remove("selected"));
+};
+
+const selectIndicator = (number) => {
+  removeClassSelected();
+  const indicator = document.querySelector(`span[data-number="${number}"]`);
+  indicator.classList.add("selected");
+};
+
+const previous = () => {
+  containerItems.appendChild(items[0]);
+  items = document.querySelectorAll(".item");
+  selectIndicator(items[1].dataset.number);
+};
+
+const next = () => {
+  const lastItem = items[items.length - 1];
+  containerItems.insertBefore(lastItem, items[0]);
+  items = document.querySelectorAll(".item");
+  selectIndicator(items[1].dataset.number);
+};
+
+const clickIndicators = ({ target }) => {
+  if (target.tagName == "SPAN") {
+    const selectedIndicator = target.dataset.number;
+    let visibleSlide = items[1].dataset.number;
+    if (selectedIndicator !== visibleSlide) {
+      const autoNext = setInterval(() => {
+        document.querySelector("#next").click();
+        visibleSlide = items[1].dataset.number;
+        if (selectedIndicator == visibleSlide) clearInterval(autoNext);
+      }, 100);
+    }
+  }
+};
+
+document.querySelector("#previous").addEventListener("click", previous);
+document.querySelector("#next").addEventListener("click", next);
+document
+  .getElementById("indicators")
+  .addEventListener("click", clickIndicators);
