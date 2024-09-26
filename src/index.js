1f663c6176f2992153eb07e3a7449962c40407ab
vinicius-omelo <viniciuss.uee@gmail.com>
2024-09-25 21:50:05 -0300

    upp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const images = [
   { id: "1", url: "./src/image/one-piece-luffy.jpg" },
-  { id: "1", url: "./src/image/one-piece-law.jpg" },
-  { id: "1", url: "./src/image/one-piece-kid.jpg" },
-  { id: "1", url: "./src/image/one-piece-teach.jpg" },
-  { id: "1", url: "./src/image/one-piece-crocodile.jpg" },
-  { id: "1", url: "./src/image/one-piece-zoro.jpg" },
-  { id: "1", url: "./src/image/one-piece-jinbe.jpg" },
-  { id: "1", url: "./src/image/one-piece-sanji.jpg" },
-  { id: "1", url: "./src/image/one-piece-katakuri.jpg" },
+  { id: "2", url: "./src/image/one-piece-law.jpg" },
+  { id: "3", url: "./src/image/one-piece-kid.jpg" },
+  { id: "4", url: "./src/image/one-piece-teach.jpg" },
+  { id: "5", url: "./src/image/one-piece-crocodile.jpg" },
+  { id: "6", url: "./src/image/one-piece-zoro.jpg" },
+  { id: "7", url: "./src/image/one-piece-jinbe.jpg" },
+  { id: "8", url: "./src/image/one-piece-katakuri.jpg" },
+  { id: "9", url: "./src/image/one-piece-sanji.jpg" },
 ];
 
-loadImages(image);
+const container = document.querySelector("#container-items");
+
+const loadImages = (images, container) => {
+  images.forEach((image) => {
+    container.innerHTML += `
+    <div class="item">
+        <img src ='${image.url}'
+    </div>
+    `;
+  });
+};
+
+loadImages(images, container);
