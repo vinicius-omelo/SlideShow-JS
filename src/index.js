424a632b3d4cda8374aa74fa70a6615847fdb552
vinicius-omelo <viniciuss.uee@gmail.com>
2024-09-25 21:40:09 -0300

    up

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const images = [
+  { id: "1", url: "./src/image/one-piece-luffy.jpg" },
+  { id: "1", url: "./src/image/one-piece-law.jpg" },
+  { id: "1", url: "./src/image/one-piece-kid.jpg" },
+  { id: "1", url: "./src/image/one-piece-teach.jpg" },
+  { id: "1", url: "./src/image/one-piece-crocodile.jpg" },
+  { id: "1", url: "./src/image/one-piece-zoro.jpg" },
+  { id: "1", url: "./src/image/one-piece-jinbe.jpg" },
+  { id: "1", url: "./src/image/one-piece-sanji.jpg" },
+  { id: "1", url: "./src/image/one-piece-katakuri.jpg" },
+];
+
+loadImages(image);
